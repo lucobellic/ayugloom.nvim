354c81f345745b3743c3289e630c463a5b8febe6
Luco Bellic <luco.bellic@protonmail.com>
2023-04-15 09:13:29 +0200

    feat(plugin): Add diffview colors

---
+++ lua/ayugloom/plugins/diffview.lua
@@ -0,0 +1,9 @@
+local lush = require('lush')
+local base = require('ayugloom.base')
+
+return lush(function()
+  return {
+    DiffviewDiffDelete  { base.Comment },
+    DiffviewDiffAddAsDelete { base.Comment },
+  }
+end)
