0ec78c4bc19dc2355b02f4afc2ff6d0fc727ec1b
Luco Bellic <luco.bellic@protonmail.com>
2023-04-21 20:38:52 +0200

    feat(plugin): add hop unmatched

--- lua/ayugloom/plugins/hop.lua
+++ lua/ayugloom/plugins/hop.lua
@@ -3,8 +3,9 @@ local c = require('ayugloom.colors')
 
 return lush(function()
   return {
-    HopNextKey  { fg = c.syntax.keyword    },
-    HopNextKey1 { fg = c.syntax.entity     },
-    HopNextKey2 { fg = lush.hsl('#C2D94C') },
+    HopUnmatched { fg = c.syntax.comment    },
+    HopNextKey   { fg = c.syntax.keyword    },
+    HopNextKey1  { fg = c.syntax.entity     },
+    HopNextKey2  { fg = lush.hsl('#C2D94C') },
   }
 end)
