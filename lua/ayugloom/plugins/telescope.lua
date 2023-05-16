d355cbeaaa36e5f67b8246adf24a91b061548923
Luco Bellic <luco.bellic@protonmail.com>
2023-05-16 20:47:28 +0200

    feat(telescope): add more telescope title colors

--- lua/ayugloom/plugins/telescope.lua
+++ lua/ayugloom/plugins/telescope.lua
@@ -1,11 +1,15 @@
 local lush = require('lush')
 local base = require('ayugloom.base')
+local c = require('ayugloom.colors')
 
 return lush(function()
   return {
     TelescopeBorder { base.FloatBorder },
-    TelescopeTitle  { base.FloatTitle  },
-    TelescopePreviewTitle  { bg = base.Identifier.fg, bold = true },
-    TelescopePromptCounter  { base.DiagnosticInfo },
+    TelescopeTitle { base.FloatTitle },
+    TelescopePromptTitle { TelescopeTitle },
+    TelescopeResultsTitle { fg = c.bg, bg = base.String.fg, bold = true },
+    TelescopePreviewTitle { fg = c.bg, bg = base.Identifier.fg, bold = true },
+    TelescopePromptCounter { base.DiagnosticInfo },
+    TelescopePreviewLine { base.Search },
   }
 end)
