01314867c59020f8c9cc5d2b0401867aa09d1169
Luco Bellic <luco.bellic@protonmail.com>
2023-05-27 00:45:33 +0200

    feat(hop): update colors

--- lua/ayugloom/plugins/hop.lua
+++ lua/ayugloom/plugins/hop.lua
@@ -4,8 +4,8 @@ local c = require('ayugloom.colors')
 return lush(function()
   return {
     HopUnmatched { fg = c.syntax.comment    },
-    HopNextKey   { fg = c.syntax.keyword    },
-    HopNextKey1  { fg = c.syntax.entity     },
-    HopNextKey2  { fg = lush.hsl('#C2D94C') },
+    HopNextKey   { fg = c.common.accent     },
+    HopNextKey1  { fg = lush.hsl('#C2D94C') },
+    HopNextKey2  { fg = c.syntax.entity.darken(20) },
   }
 end)
