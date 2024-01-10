32eaf8e1e6de378ac2620e5168c30b6e1371d46c
Luco Bellic <luco.bellic@protonmail.com>
2024-01-10 18:00:08 +0100

    feat(plugins): add support for satellite.nvim

---
+++ lua/ayugloom/plugins/satellite.lua
@@ -0,0 +1,10 @@
+local lush = require('lush')
+local c = require('ayugloom.colors')
+local base = require('ayugloom.base')
+
+return lush(function()
+  return {
+    SatelliteMark { base.NonText },
+    SatelliteBar { base.DiffText },
+  }
+end)
