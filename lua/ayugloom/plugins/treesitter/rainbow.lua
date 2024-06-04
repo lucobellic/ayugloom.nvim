df8621665d1fe038fa9948af8d9b7c56c21f57b5
Luco Bellic <luco.bellic@protonmail.com>
2024-06-04 22:20:11 +0200

    feat(rainbow): update rainbow colorscheme

--- lua/ayugloom/plugins/treesitter/rainbow.lua
+++ lua/ayugloom/plugins/treesitter/rainbow.lua
@@ -1,16 +1,15 @@
 local lush = require('lush')
 local c = require('ayugloom.colors')
-local base = require('ayugloom.base')
 
 return lush(function()
-  return {
-    TSRainbowRed { fg = base.ErrorMsg.fg },
-    TSRainbowYellow { base.Keyword },            -- Gold
-    TSRainbowBlue { base.Identifier },           -- LightSkyBlue
-    TSRainbowOrange { base.Operator },           -- DarkOrange
-    TSRainbowGreen { base.String },
+  return                {
+    TSRainbowRed        { fg = c.red },
+    TSRainbowYellow     { fg = c.yellow }, -- Gold
+    TSRainbowBlue       { fg = c.blue },   -- LightSkyBlue
+    TSRainbowOrange     { fg = c.orange }, -- DarkOrange
+    TSRainbowGreen      { fg = c.green },
     TSRainbowLightGreen { fg = c.syntax.regexp },
-    TSRainbowViolet { fg = c.syntax.parameter }, -- Orchid
-    TSRainbowCyan { fg = c.syntax.namespace },
+    TSRainbowViolet     { fg = c.magenta }, -- Orchid
+    TSRainbowCyan       { fg = c.cyan },
   }
 end)
